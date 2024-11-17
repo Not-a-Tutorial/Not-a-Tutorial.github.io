006722a18991468af44f49f859cc8415f25cc95d
Not-a-Tutorial <122500342+Not-a-Tutorial@users.noreply.github.com>
2024-11-16 22:08:18 -0500

    Update header.js

--- header.js
+++ header.js
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", function() {
   fetch('/footer.html')
     .then(response => response.text())
     .then(data => {
-      document.getElementByID('footer-placeholder').innerHTML = data;
+      document.getElementById('footer-placeholder').innerHTML = data;
     })
     .catch(error => console.error('Error loading footer:', error));
 });
